24978babc735292671f453c4a4539f750e24157f
Nguyen Hieu <tthzicc@gmail.com>
2019-12-05 20:46:06 +0700

    Add memu-lv2 for site header nav

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
 	$("#menu-toggle").click(function() {
+		$(".overlay").addClass("overlay-toggle");
 		$(".site-nav-action").addClass("active");
 		$(".site-nav-action").removeClass("close");
 	});
 	$("#close-menu").click(function () {
+		$(".overlay").removeClass("overlay-toggle");
 		$(".site-nav-action").addClass("close");
 		$(".site-nav-action").removeClass("active");
+	});
+})
+
+
+
+$(document).ready(function () {
+	$("#close-menu-lv2").click(function() {
+		$(".nav-lv2-wrapper").addClass("display-none");
 	});
 })

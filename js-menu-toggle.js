9f56bb68e9273b36919d43aa42255f5ee1d3676b
Nguyen Hieu <tthzicc@gmail.com>
2019-12-08 08:31:10 +0700

    Done show-hide menu function with screen > 992px

--- js-menu-toggle.js
+++ js-menu-toggle.js
@@ -4,20 +4,21 @@ $(document).ready(function () {
 		$(".site-nav-action").addClass("active");
 		$(".site-nav-action").removeClass("close");
 	});
+
 	$("#close-menu").click(function () {
 		$(".overlay").removeClass("overlay-toggle");
 		$(".site-nav-action").addClass("close");
 		$(".site-nav-action").removeClass("active");
 	});
+
+	$("nav.items .item > a").click(function() {
+		$(this).parent().toggleClass("active-block").siblings().removeClass("active-block");
+	});
 })
 
-
-
-$(document).ready(function () {
-	$("nav.items .item").click(function() {
-		$(this).toggleClass("active-block").siblings().removeClass("active-block");
-	});
+$(document).ready(function() {
+	$(".close-menu-lv2").click(function() {
+		$("nav.items .item").removeClass("active-block");
+	})
 });
-
-
 

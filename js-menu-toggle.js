3dbf2466949f697123eb64c88b94faf36f1e114c
Nguyen Hieu <tthzicc@gmail.com>
2019-12-09 10:09:30 +0700

    done site header

--- js-menu-toggle.js
+++ js-menu-toggle.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
 	$("#menu-toggle").click(function() {
+		$(".wrapper").addClass("body-height");
 		$(".overlay").addClass("overlay-toggle");
 		$(".site-nav-action").addClass("active");
 		$(".site-nav-action").removeClass("close");
 	});
 
 	$("#close-menu").click(function () {
+		$(".wrapper").removeClass("body-height");
 		$(".overlay").removeClass("overlay-toggle");
 		$(".site-nav-action").addClass("close");
 		$(".site-nav-action").removeClass("active");
@@ -21,4 +23,12 @@ $(document).ready(function() {
 		$("nav.items .item").removeClass("active-block");
 	})
 });
+
+$(document).ready(function() {
+	$(".overlay").click(function() {
+		$(".overlay").removeClass("overlay-toggle");
+		$(".site-nav-action").removeClass("active").addClass("close");
+		$(".wrapper").removeClass("body-height");
+	})
+})
 
